0adbae58218212f4a56b0ead4aa56acdc07f3587
Gille Testard <g.testard@galites.net>
2022-04-08 15:43:07 +0200

    update step 2 blog/index.svelte fetch in context

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,23 +1,39 @@
 <script context="module">
     import r from '$lib/helpers/req'
-    export const load = async () => {
-        // const {blog, url} = await r.getAllPosts()
-        // console.log('blog loading : ')
+    export const load = async ({fetch}) => {
+        const res = await fetch('https://2yufqfqe.directus.app/graphql', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            query: `
+            query {
+                blog  (filter: {status: {_eq: "published"}}) {
+                    id
+                    title
+                    date_created
+                    date_updated
+                }
+            }
+            `
+        })
+    })
+    const {data: {blog}} = await res.json()
         return {
             props: {
-                arr: []
+                blog
             }
         }
     }
 </script>
 <script>
     export let blog = []
-    export let arr = []
 </script>
 <h1>Index du blog</h1>
-{#if arr.length > 0}
+{#if blog.length > 0}
     <ul>
-     {#each arr as {id, title}}
+     {#each blog as {id, title}}
           <li><a sveltekit:prefetch href="/blog/{id}" {title}>{title}</a></li>
      {/each}
     </ul>
