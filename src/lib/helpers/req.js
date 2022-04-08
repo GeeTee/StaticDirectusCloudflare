81c6ac2ec591989b8e581d6464bb5c48847872b6
Gille Testard <g.testard@galites.net>
2022-04-08 15:58:02 +0200

    update req.js url clair / item

--- src/lib/helpers/req.js
+++ src/lib/helpers/req.js
@@ -30,7 +30,7 @@ const getAllPosts = async () => {
 }
 
 const getPostById = async (id) => {
-    const res = await fetch(`${url}/graphql`, {
+    const res = await fetch('https://2yufqfqe.directus.app/graphql', {
         method: 'POST',
         headers: {
             "Content-Type": "application/json"
