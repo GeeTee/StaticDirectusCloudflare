f72dc18f5e2f6cf57e496cdc5b1b3d5f3e91572f
Gille Testard <g.testard@galites.net>
2022-04-12 11:06:02 +0200

    category pagination

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -10,14 +10,36 @@
     }
 </script>
 <script>
+    import {
+    paginate,
+    DarkPaginationNav
+    } from 'svelte-paginate'
+    import { fade } from 'svelte/transition'
     export let blog = []
     console.log('expected blog', blog)
+    const items = [...blog]
+    let currentPage = 1
+    let pageSize = 2
+    $: paginatedItems = paginate({ items, pageSize, currentPage })
 </script>
 <h1>Index du blog</h1>
 {#if blog.length > 0}
     <ul>
-     {#each blog as {id, title}}
-          <li><a sveltekit:prefetch href="/blog/{id}" {title}>{title}</a></li>
+     {#each paginatedItems as {id, title} (id)}
+        <li
+        out:fade="{{duration: 200}}"
+        in:fade="{{delay: 200}}"
+        >
+            <a sveltekit:prefetch href="/blog/{id}" {title}>{title}</a>
+        </li>
      {/each}
     </ul>
+    <DarkPaginationNav
+        currentPage={currentPage}
+        pageSize={pageSize}
+        totalItems={items.length}
+        limit="{1}"
+        showStepOptions="{true}"
+        on:setPage={(e) => currentPage = e.detail.page}
+    />
 {/if}
